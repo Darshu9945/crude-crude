75f788bde33d358b596f8d7225d34af1cc921423
Darshu9945 <1ms18ch016@gmail.com>
2023-03-05 01:12:14 +0530

    added

--- networkapi.js
+++ networkapi.js
@@ -17,7 +17,7 @@ btn.addEventListener('submit',adduser);
             name,
             email
         }
-        axios.post("https://crudcrud.com/api/34ba1a7f54314ee4b16f68e239f5f044/appoitmentdata",obj).then((responce)=>{
+        axios.post("https://crudcrud.com/api/7cda50fa700440ee9515a261ad723318/appoitmentdata",obj).then((responce)=>{
         }
         ).catch((err)=>{
             console.log(err)
@@ -25,15 +25,13 @@ btn.addEventListener('submit',adduser);
         
  }
 
-    axios.get("https://crudcrud.com/api/34ba1a7f54314ee4b16f68e239f5f044/appoitmentdata")
+    axios.get("https://crudcrud.com/api/7cda50fa700440ee9515a261ad723318/appoitmentdata")
     .then((response)=>{
-        console.log(response.data[0].name)
         for (let i=0; i<response.data.length; i++){
             shownewuseronscreen(response.data[i])
         }
     })
 function shownewuseronscreen(user){
-    console.log("darshankb")
     const li=document.createElement('li')
     li.textContent=user.name+'-'+ user.email
     console.log(li)
